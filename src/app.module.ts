d1e8b538b27f0910f99b980f2096cc858aac366f
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2025-01-26 14:46:47 +0530

    Add AuthModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ServersModule } from './modules/servers/servers.module';
 import { ContactsModule } from './modules/contacts/contacts.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { NotificationsModule } from './modules/notifications/notifications.modul
     ServersModule,
     ContactsModule,
     NotificationsModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
