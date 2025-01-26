4689f31499fed0d5032c2a78c55c7e9147298e0b
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2025-01-26 12:57:51 +0530

    Add Swagger documentation setup and global validation pipe to the application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Alert System API')
+    .setDescription('API documentation for the Alert System')
+    .setVersion('1.0')
+    .addTag('servers')
+    .addTag('contacts')
+    .addTag('notifications')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
