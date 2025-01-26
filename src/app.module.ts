791c43a5051f9e5684427331b1c4d785c9691b5c
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2025-01-26 14:57:50 +0530

    Update MongoDB connection string to use localhost IP address

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/alert-system'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/alert-system'),
     ServersModule,
     ContactsModule,
     NotificationsModule,
